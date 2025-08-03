5d81f66a253666056ebed1a8105cbd1906dc2cf5
Anshuk Jirli <anshuk.jirli2002@gmail.com>
2025-08-04 02:23:48 +0530

    Fixed incorrect imports in dependecies.py files of each microservice

--- user_service/app/dependencies.py
+++ user_service/app/dependencies.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
 from . import database
-import sys
-import os
+# import sys # Remove this line
+# import os # Remove this line
 
-# Add the parent directory to sys.path to find the shared module
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+# Add the parent directory to sys.path to find the shared module # Remove this comment
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))) # Remove this line
 
 from shared.service_client import ServiceClient
 from shared.config import RESTAURANT_SERVICE_URL, DELIVERY_AGENT_SERVICE_URL
